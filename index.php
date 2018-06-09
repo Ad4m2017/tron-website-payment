ff94cbdf5bc61ec02579e12f94107c7ef385c4e5
4dam2017 <40077537+4dam2017@users.noreply.github.com>
2018-06-09 05:59:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 echo "Hello Tron World!";
 
-$response = file_get_conteznts('https://api.tronscan.org/api/account/TGxyQ8Z9xK57HDPZe1LWbBXAuQfT92xS5Y');
+$response = file_get_contents('https://api.tronscan.org/api/account/TGxyQ8Z9xK57HDPZe1LWbBXAuQfT92xS5Y');
 
 echo $response;
 

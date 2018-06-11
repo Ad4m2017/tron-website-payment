f54ecffe63758cdf1fcfc5a74f1980a089a5580b
Ad4m2017 <d3geee@gmail.com>
2018-06-11 09:53:40 +0200

    update

--- database-mysql.php
+++ database-mysql.php
@@ -17,7 +17,7 @@ $db = mysqli_connect("localhost","root","","db_tron_pay");
 /*	Check connection	*/
 if ( mysqli_connect_errno() )
 {
-    $status		= "error";
+	$status		= "error";
 	$message	= "Failed to connect to MySQL: " . mysqli_connect_error();
 	return json_output( $status, $message );    
 }

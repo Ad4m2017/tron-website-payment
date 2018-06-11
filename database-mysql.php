2ef1945ef543c57e7551bf2816282fa54759042a
Adam <40077537+Ad4m2017@users.noreply.github.com>
2018-06-11 08:29:02 +0200

    Update database-mysql.php

--- database-mysql.php
+++ database-mysql.php
@@ -1,7 +1,16 @@
 <?php
 /*
-    
+    all queryoutput in json for
 */
 
+$db = mysqli_connect("localhost","root","","db_tron_pay");
 
+/*
+    Check connection
+*/
+if ( mysqli_connect_errno() )
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    
+}
 ?>

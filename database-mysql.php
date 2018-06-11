9af8b6cf4322e39501891bfd034326e95b8b9b0c
Adam <40077537+Ad4m2017@users.noreply.github.com>
2018-06-11 09:09:54 +0200

    Update database-mysql.php

--- database-mysql.php
+++ database-mysql.php
@@ -1,7 +1,16 @@
 <?php
 /*
-    all queryoutput in json for
+    database-mysql.php return only in json format
 */
+
+
+$json->status = "0";
+$json->text = "";
+
+$json = json_encode($json);
+
+echo $json;
+
 
 $db = mysqli_connect("localhost","root","","db_tron_pay");
 

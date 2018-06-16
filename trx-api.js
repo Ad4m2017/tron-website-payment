aa2dc0059d131556efa30fdf4f7628907a300170
Ad4m2017 <d3geee@gmail.com>
2018-06-16 19:30:55 +0200

    update...

--- trx-api.js
+++ trx-api.js
@@ -3,7 +3,7 @@
     Contact: https://t.me/Ad4m2017
 */
 
-
+/*
 // Assign handlers immediately after making the request,
 // and remember the jqxhr object for this request
 var jqxhr = $.post( "https://api.tronscan.org/api/market/markets", function() {
@@ -25,3 +25,4 @@ var jqxhr = $.post( "https://api.tronscan.org/api/market/markets", function() {
 jqxhr.always(function() {
   alert( "second finished" );
 });
+*/
